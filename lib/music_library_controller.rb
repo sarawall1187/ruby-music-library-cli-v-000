ca0fd580f7795bdf8c19f4460fedc74a3eef8cff
Sara Walker <sarawall1187@gmail.com>
2019-01-22 18:56:58 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -85,13 +85,21 @@ class MusicLibraryController
    end
   end 
   
+  # def play_song
+  #   puts "Which song number would you like to play?"
+  #   input = gets.strip.to_i - 1
+  #   if input != nil 
+  #     song_choice = list_songs[input]
+  #     puts "Playing #{song_choice.name} by #{song_choice.artist.name}"
+  # end
+  # end
   def play_song
     puts "Which song number would you like to play?"
     input = gets.strip.to_i - 1
+    songs = Song.all.sort {|a, b| a.name <=> b.name}
      if input != nil 
-      song_choice = list_songs[input]
+      song_choice = songs[input]
       puts "Playing #{song_choice.name} by #{song_choice.artist.name}"
    end
   end
-  
 end
